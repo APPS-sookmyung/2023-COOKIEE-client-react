74bf4d50031fa8d25a97e976fe464e505da9c92a
mxinseo <jade201913@gmail.com>
2023-07-30 00:20:34 +0900

    [Feat] 캘린더 홈화면 구현

---
+++ CookieeApp/screens/CalenderHome.js
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from "react-native";
+import Calendar from "../components/Calender";
+
+function CalendarHome() {
+  return (
+    <View style={S.calendarContainer}>
+      <Calendar />
+    </View>
+  );
+}
+
+const S = StyleSheet.create({
+  calendarContainer: {
+    flex: 1,
+  },
+});
+
+export default CalendarHome;
