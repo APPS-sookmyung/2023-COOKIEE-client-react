d9d15ac3a157095ec1b97b507fca3f7ac12f35a9
mxinseo <jade201913@gmail.com>
2023-09-24 17:58:44 +0900

    [Feat] AddNewEvent.js 파일 새로 생성
    EventImagePicker->AddNewEvent(파일 이동, 파일명 수정)

---
+++ CookieeApp/screens/AddNewEvent.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { TouchableOpacity, Image, View, Text, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+export default function AddNewEvent({ onPressed }) {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      onImagesSelected(result.assets[0].uri); // 선택한 이미지의 uri를 부모 컴포넌트로 전달
+    }
+  };
+
+  return (
+    <View style={styles.AddNewEventScreen}>
+      <Text>새로운 이벤트 추가</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
