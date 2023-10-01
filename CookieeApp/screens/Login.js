b701c317945d01077e88884b00af3f10af1664b6
mxinseo <jade201913@gmail.com>
2023-10-01 14:48:22 +0900

    [Feat] ios빌드 실패 오류 해결
        iosClientId: "YOUR_IOS_CLIENT_ID",
        androidClientId: "YOUR_ANDROID_CLIENT_ID",
    
    빈칸이었는데 다음과 같이 채워 넣었습니다.

--- CookieeApp/screens/Login.js
+++ CookieeApp/screens/Login.js
@@ -1,79 +1,74 @@
 // import GoogleButton from 'react-google-button';
 // import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
-import * as React from 'react';
-import * as WebBrowser from 'expo-web-browser';
-import * as Google from 'expo-auth-session/providers/google';
+import * as React from "react";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
 import {
   View,
   Text,
   Button,
   StyleSheet,
   Image,
-  TouchableOpacity
-} from 'react-native';
+  TouchableOpacity,
+} from "react-native";
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function Login() {
   const [accessToken, setAccessToken] = React.useState(null);
   const [user, setUser] = React.useState(null);
-  const [request, response, promptAsync] = Google.useIdTokenAuthRequest((
-    iosClientId: '',
-    androidClientId: '',
-  )); 
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    iosClientId: "YOUR_IOS_CLIENT_ID",
+    androidClientId: "YOUR_ANDROID_CLIENT_ID",
+  });
 
-  React.useEffect(()=> {
-    if(response?.type === "success"){
+  React.useEffect(() => {
+    if (response?.type === "success") {
       setAccessToken(response.authentication);
       accessToken && fetchUserInfo(); // accessToken 필요
     }
-  }, [response, accessToken])
+  }, [response, accessToken]);
 
-  async function fetchUserInfo(){
-    let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {// endpoint
-      headers: { Authorization: `Bearer ${accessToken}` }
+  async function fetchUserInfo() {
+    let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+      // endpoint
+      headers: { Authorization: `Bearer ${accessToken}` },
     });
     const useInfo = await response.json();
     setUser(useInfo);
   }
 
   const ShowUserInfo = () => {
-    if(user){
-      return(
-        <View style={{alignItems: 'center', justifyContent: 'center'}}>
-          
-        </View>
-      )
+    if (user) {
+      return (
+        <View style={{ alignItems: "center", justifyContent: "center" }}></View>
+      );
     }
-  }
+  };
 
   return (
     <View>
       <View style={styles.container}>
-        <Image
-          style={styles.image}
-          source={require('../assets/cookie.png')}
-        />
-        <Text style={styles.title_text}>
-          Cookiee
-        </Text>
-        <Text style={styles.content_text1}>
-          오늘 하루를 사진으로 기록해 </Text>
-          <Text style={styles.content_text2}>
-          나만의 쿠키를 만들어보아요 </Text>
+        <Image style={styles.image} source={require("../assets/cookie.png")} />
+        <Text style={styles.title_text}>Cookiee</Text>
+        <Text style={styles.content_text1}>오늘 하루를 사진으로 기록해 </Text>
+        <Text style={styles.content_text2}>나만의 쿠키를 만들어보아요 </Text>
       </View>
       <View style={styles.container}>
-        {user && <ShowUserInfo/>}
-        {user === null &&
+        {user && <ShowUserInfo />}
+        {user === null && (
           <TouchableOpacity
             disabled={!request}
             onPress={() => {
               promptAsync();
             }}
           >
-            <Image source={require("../assets/btn_google.png")} style={{width: 300, height: 20}}></Image>
+            <Image
+              source={require("../assets/btn_google.png")}
+              style={{ width: 300, height: 20 }}
+            ></Image>
           </TouchableOpacity>
-        }
+        )}
       </View>
     </View>
   );
@@ -97,7 +92,7 @@ const styles = StyleSheet.create({
   title_text: {
     color: "#594E4E",
     fontSize: 40,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     paddingBottom: 13,
   },
   content_text1: {
