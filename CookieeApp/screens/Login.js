6b66e74501580e729a240a5ab108a3d2ac76b276
윤서희 <appleid21@sookmyung.ac.kr>
2023-10-02 22:21:00 +0900

    Fix: 로그인, 입력화면 수정

--- CookieeApp/screens/Login.js
+++ CookieeApp/screens/Login.js
@@ -1,74 +1,54 @@
 // import GoogleButton from 'react-google-button';
 // import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
-import * as React from "react";
-import * as WebBrowser from "expo-web-browser";
-import * as Google from "expo-auth-session/providers/google";
+import * as React from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
 import {
   View,
   Text,
   Button,
   StyleSheet,
-  Image,
-  TouchableOpacity,
-} from "react-native";
+  Image
+} from 'react-native';
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function Login() {
-  const [accessToken, setAccessToken] = React.useState(null);
-  const [user, setUser] = React.useState(null);
-  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    iosClientId: "YOUR_IOS_CLIENT_ID",
-    androidClientId: "YOUR_ANDROID_CLIENT_ID",
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    iosClientId: '750908582355-4i3spir7ue0gj7n9l8afhfsp2iuv8m7e.apps.googleusercontent.com',
+    androidClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+    responseType: 'id_token'
   });
 
   React.useEffect(() => {
-    if (response?.type === "success") {
-      setAccessToken(response.authentication);
-      accessToken && fetchUserInfo(); // accessToken 필요
+    if (response?.type === 'success') {
+      const { authentication } = response;
     }
-  }, [response, accessToken]);
-
-  async function fetchUserInfo() {
-    let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
-      // endpoint
-      headers: { Authorization: `Bearer ${accessToken}` },
-    });
-    const useInfo = await response.json();
-    setUser(useInfo);
-  }
-
-  const ShowUserInfo = () => {
-    if (user) {
-      return (
-        <View style={{ alignItems: "center", justifyContent: "center" }}></View>
-      );
-    }
-  };
+  }, [response]);
 
   return (
     <View>
       <View style={styles.container}>
-        <Image style={styles.image} source={require("../assets/cookie.png")} />
-        <Text style={styles.title_text}>Cookiee</Text>
-        <Text style={styles.content_text1}>오늘 하루를 사진으로 기록해 </Text>
-        <Text style={styles.content_text2}>나만의 쿠키를 만들어보아요 </Text>
+        <Image
+          style={styles.image}
+          source={require('../assets/cookie.png')}
+        />
+        <Text style={styles.title_text}>
+          Cookiee
+        </Text>
+        <Text style={styles.content_text1}>
+          오늘 하루를 사진으로 기록해 </Text>
+          <Text style={styles.content_text2}>
+          나만의 쿠키를 만들어보아요 </Text>
       </View>
-      <View style={styles.container}>
-        {user && <ShowUserInfo />}
-        {user === null && (
-          <TouchableOpacity
-            disabled={!request}
-            onPress={() => {
-              promptAsync();
-            }}
-          >
-            <Image
-              source={require("../assets/btn_google.png")}
-              style={{ width: 300, height: 20 }}
-            ></Image>
-          </TouchableOpacity>
-        )}
+      <View style={styles.button_style}>
+        <Button
+          disabled={!request}
+          title="Google 로그인하기"
+          onPress={() => {
+            promptAsync();
+          }}
+        />
       </View>
     </View>
   );
@@ -92,7 +72,7 @@ const styles = StyleSheet.create({
   title_text: {
     color: "#594E4E",
     fontSize: 40,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     paddingBottom: 13,
   },
   content_text1: {
