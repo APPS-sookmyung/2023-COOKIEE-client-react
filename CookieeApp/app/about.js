eeaf3d29109804ea773072a2e118ee9a956e09ee
mxinseo <jade201913@gmail.com>
2023-11-10 01:30:38 +0900

    Feat: Expo Router 실험

---
+++ CookieeApp/app/about.js
@@ -0,0 +1,10 @@
+import { Pressable, Text } from "react-native";
+import { Link } from "expo-router";
+
+export default function about() {
+  return (
+    <Pressable>
+      <Text>about</Text>
+    </Pressable>
+  );
+}
