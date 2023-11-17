0d7c71807e126492874d1e154bd87e27cf3234ad
mxinseo <jade201913@gmail.com>
2023-11-17 18:33:02 +0900

    [Del] 불필요한 주석부분 삭제

--- CookieeApp/app/(tabs)/home.js
+++ CookieeApp/app/(tabs)/home.js
@@ -218,8 +218,6 @@ function Body(props) {
                   resizeMode="cover"
                 >
                   <TouchableOpacity
-                    // href={"/DayBottomModal"}
-                    // onPress={handlePressDay.bind(this, checkPressedDate)}
                     onPress={() => handlePressDay(checkPressedDate)}
                     style={
                       (({ pressed }) => {
@@ -258,17 +256,6 @@ function Body(props) {
           })
         )}
       </View>
-      {/* <DayBottomModal
-        isVisible={isModalVisible}
-        onClose={() => setIsModalVisible(false)}
-        selectedDate={selectedDate}
-        getSelectedImageUris={getSelectedImageUris}
-      /> */}
-      {/* <Text>
-        {selectedDate && selectedDate.date
-          ? selectedImageUris[selectedDate.date]
-          : null}
-      </Text> */}
     </View>
   );
 }
