4c37759e29042bd9a5d8365ada858609ec5fc7f5
mxinseo <jade201913@gmail.com>
2023-12-24 17:38:51 +0900

    Feat: axios 카테코리 조회 테스트 성공

--- CookieeApp/app/components/EventBox.js
+++ CookieeApp/app/components/EventBox.js
@@ -1,6 +1,30 @@
 import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function EventBox() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("react");
+
+  //useEffect는 비동기적으로 동작
+  useEffect(() => {
+    let completed = false; //초기에는 실행해야 되기때문에 false flag 변수
+
+    //query를 리턴하는 함수를 result에 할당
+    async function get() {
+      const result = await axios.get(`http://cookiee.site/category/1`);
+      if (!completed) {
+        setData(result.data);
+        console.log(result.data.result);
+      }
+    }
+    get();
+    return () => {
+      completed = true;
+    };
+    //query가 변할때 useEffect를 실행해야하는 시점이다
+  }, [query]); //input에 값이 변경이 되었을때 effect를 실행한다
+
   return (
     <View style={styles.AddEventContainer}>
       <View style={styles.FirstEventImageContainer}>
