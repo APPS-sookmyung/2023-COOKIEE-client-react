ce040876e8aea2abee6550497afa615dac5691f4
mxinseo <jade201913@gmail.com>
2024-01-27 21:55:42 +0900

    Feat: 개별 날짜 썸네일 등록 api 연결

---
+++ CookieeApp/api/thumbnail/getThumb.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const getThumb = async (userId) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:8080/thumbnail/view/${userId}`
+    );
+
+    if (response.status !== 200) {
+      throw new Error("Network response was not ok");
+    }
+
+    return response.data.result; // 업데이트된 데이터 반환 또는 처리
+  } catch (error) {
+    console.error("Error get thumbnail data:", error);
+    return null; // 에러 처리 또는 다른 방식으로 처리
+  }
+};
