471d3337513d00b9b687e855fc3c7a905c9724cf
mxinseo <jade201913@gmail.com>
2024-02-04 17:33:05 +0900

    Feat: 이벤트 id 별 조회 api 연결

--- CookieeApp/app/(tabs)/event/[eventid].js
+++ CookieeApp/app/(tabs)/event/[eventid].js
@@ -7,51 +7,44 @@ import {
   TouchableOpacity,
   Alert,
 } from "react-native";
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import { EvilIcons } from "@expo/vector-icons";
 import Carousel from "react-native-reanimated-carousel";
 
-import getCate from "../../../api/category/getCate";
-import { useRouter, useLocalSearchParams } from "expo-router";
+import { getEventById } from "../../../api/event/getEventById";
+import { useRouter, useLocalSearchParams, useFocusEffect } from "expo-router";
 
 const EventDetailIndex = () => {
   const router = useRouter();
 
   const { eventid } = useLocalSearchParams();
 
-  const [cate, setCate] = useState([]);
+  const [eventData, setEventData] = useState([]);
+  const [eventImgData, setEventImgData] = useState([]);
   const [userId, setUserId] = useState(1);
 
-  useEffect(() => {
-    let completed = false; // 첫 번째 1회 실행을 위한 flag
+  async function handelGetEventById() {
+    console.log("handelGetEventById 실행");
+    try {
+      const event = await getEventById(userId, eventid);
 
-    async function get() {
-      try {
-        const result = await getCate(userId);
-        if (!completed) {
-          if (result != null) {
-            setCate(result);
-            console.log(result);
-          }
-        }
-      } catch (error) {
-        console.log(error);
+      if (event != null) {
+        setEventData(event);
+        setEventImgData(event.imageUrlList);
+        console.log("event: ", await event);
       }
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    get();
-    return () => {
-      completed = true;
-    };
-  }, [userId]); // userId가 변경될 때 마다 실행
+  useFocusEffect(
+    useCallback(() => {
+      handelGetEventById();
+    }, [])
+  );
 
   const width = Dimensions.get("window").width;
-
-  const data = [
-    require("../../../assets/testImage/test.jpeg"),
-    require("../../../assets/testImage/test2.jpeg"),
-    require("../../../assets/testImage/test3.jpeg"),
-  ];
 
   const deleteEvent = () => {
     console.log("이벤트 삭제 api");
@@ -75,108 +68,117 @@ const EventDetailIndex = () => {
     );
   };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.headerSection}>
-        <View>
-          <Text style={{ fontSize: 27, fontWeight: 600 }}>2023.12.20(수)</Text>
-        </View>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <TouchableOpacity style={styles.iconContainer}>
-            <EvilIcons name="pencil" size={37} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.iconContainer} onPress={deleteEvent}>
-            <EvilIcons name="trash" size={35} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.iconContainer}
-            onPress={() => router.back()}
+  if (eventData.eventId != null) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.headerSection}>
+          <View>
+            <Text style={{ fontSize: 27, fontWeight: 600 }}>
+              2023.12.20(수)
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
           >
-            <EvilIcons name="close" size={33} color="black" />
-          </TouchableOpacity>
+            <TouchableOpacity style={styles.iconContainer}>
+              <EvilIcons name="pencil" size={37} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.iconContainer}
+              onPress={deleteEvent}
+            >
+              <EvilIcons name="trash" size={35} color="black" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.iconContainer}
+              onPress={() => router.back()}
+            >
+              <EvilIcons name="close" size={33} color="black" />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-      <View style={styles.imageSection}>
-        <Carousel
-          loop
-          mode="parallax"
-          width={width}
-          autoPlay={false}
-          data={data}
-          scrollAnimationDuration={2000}
-          onSnapToItem={(index) => console.log("current index:", index)}
-          renderItem={({ item, index }) => (
-            <View style={{ flex: 1, justifyContent: "center" }}>
-              <Image
-                source={item}
-                style={{
-                  width: width,
-                  height: "100%",
-                  resizeMode: "cover",
-                  borderRadius: 7,
-                }}
-              />
-            </View>
-          )}
-        />
-      </View>
-      <View style={styles.contentSection}>
-        <View style={styles.contentContainer}>
-          <View style={styles.contentTitleContainer}>
-            <Text style={styles.contentTitle}>{"카테고리"}</Text>
-            <View style={styles.EventInfoCategoryBox}>
-              <View style={styles.EventInfo}>
-                {cate.map((category, index) => (
-                  <View
-                    key={index}
-                    style={{
-                      ...styles.EventInfoCategoryBox,
-                      backgroundColor: category.categoryColor,
-                      marginRight: 10,
-                    }}
-                  >
-                    <Text style={styles.EventInfoCategoryText}>
-                      #{category.categoryName}
-                    </Text>
-                  </View>
-                ))}
+        <View style={styles.imageSection}>
+          <Carousel
+            loop
+            mode="parallax"
+            width={width}
+            autoPlay={false}
+            data={eventImgData}
+            scrollAnimationDuration={2000}
+            onSnapToItem={(index) => console.log("current index:", index)}
+            renderItem={({ item, index }) => (
+              <View style={{ flex: 1, justifyContent: "center" }}>
+                <Image
+                  source={{ uri: item }}
+                  style={{
+                    width: width,
+                    height: "100%",
+                    resizeMode: "cover",
+                    borderRadius: 7,
+                  }}
+                />
+              </View>
+            )}
+          />
+        </View>
+        <View style={styles.contentSection}>
+          <View style={styles.contentContainer}>
+            <View style={styles.contentTitleContainer}>
+              <Text style={styles.contentTitle}>{"카테고리"}</Text>
+              <View style={styles.EventInfoCategoryBox}>
+                <View style={styles.EventInfo}>
+                  {eventData.categories.map((category, index) => (
+                    <View
+                      key={index}
+                      style={{
+                        ...styles.EventInfoCategoryBox,
+                        backgroundColor: category.categoryColor,
+                        marginRight: 10,
+                      }}
+                    >
+                      <Text style={styles.EventInfoCategoryText}>
+                        #{category.categoryName}
+                      </Text>
+                    </View>
+                  ))}
+                </View>
               </View>
             </View>
           </View>
-        </View>
-        <View style={styles.contentContainer}>
-          <View style={styles.contentTitleContainer}>
-            <Text style={styles.contentTitle}>{"시간"}</Text>
-            <Text style={styles.contentDetail}>13:00-18:00</Text>
+          <View style={styles.contentContainer}>
+            <View style={styles.contentTitleContainer}>
+              <Text style={styles.contentTitle}>{"시간"}</Text>
+              <Text style={styles.contentDetail}>
+                {eventData.startTime}-{eventData.endTime}
+              </Text>
+            </View>
           </View>
-        </View>
-        <View style={styles.contentContainer}>
-          <View style={styles.contentTitleContainer}>
-            <Text style={styles.contentTitle}>{"장소"}</Text>
-            <Text style={styles.contentDetail}>제주도</Text>
+          <View style={styles.contentContainer}>
+            <View style={styles.contentTitleContainer}>
+              <Text style={styles.contentTitle}>{"장소"}</Text>
+              <Text style={styles.contentDetail}>{eventData.eventWhere}</Text>
+            </View>
           </View>
-        </View>
-        <View style={styles.contentContainer}>
-          <View style={styles.contentTitleContainer}>
-            <Text style={styles.contentTitle}>{"내용"}</Text>
-            <Text style={styles.contentDetail}>통귤 탕후루와 제주 꽃</Text>
+          <View style={styles.contentContainer}>
+            <View style={styles.contentTitleContainer}>
+              <Text style={styles.contentTitle}>{"내용"}</Text>
+              <Text style={styles.contentDetail}>{eventData.what}</Text>
+            </View>
           </View>
-        </View>
-        <View style={styles.contentContainer}>
-          <View style={styles.contentTitleContainer}>
-            <Text style={styles.contentTitle}>{"함께한 사람"}</Text>
-            <Text style={styles.contentDetail}>동기들</Text>
+          <View style={styles.contentContainer}>
+            <View style={styles.contentTitleContainer}>
+              <Text style={styles.contentTitle}>{"함께한 사람"}</Text>
+              <Text style={styles.contentDetail}>{eventData.withWho}</Text>
+            </View>
           </View>
         </View>
       </View>
-    </View>
-  );
+    );
+  }
 };
 
 export default EventDetailIndex;
