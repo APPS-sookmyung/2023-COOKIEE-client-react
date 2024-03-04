3f89290419d5324379176d34d6887b6ed077874a
mxinseo <jade201913@gmail.com>
2024-03-05 01:41:37 +0900

    Fix: 카테고리 조회 헤더 토큰 추가

--- CookieeApp/api/category/getCate.js
+++ CookieeApp/api/category/getCate.js
@@ -2,7 +2,15 @@ import axios from "axios";
 
 export const getCate = async (userId) => {
   try {
-    const response = await axios.get(`https://cookiee.site/category/${userId}`);
+    const response = await axios.get(
+      `https://cookiee.site/category/${userId}`,
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMiIsInJvbGUiOiJST0xFX1VTRVIiLCJpYXQiOjE3MDk1MTM4NzcsImV4cCI6MTcxMjEwNTg3N30.ZHC6ZPw6WsTfMR7at4FLkLAjNDU0vOMgfWL1lI3DsOs",
+        },
+      }
+    );
 
     if (response.status !== 200) {
       throw new Error("Network response was not ok");
