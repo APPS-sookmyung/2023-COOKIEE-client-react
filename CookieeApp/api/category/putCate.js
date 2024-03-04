5e2f8458e0ea2016a6905c0c5ec96b5b16c5e5d0
mxinseo <jade201913@gmail.com>
2024-03-05 01:57:37 +0900

    Fix: 카테고리 수정 헤더 토큰 추가

--- CookieeApp/api/category/putCate.js
+++ CookieeApp/api/category/putCate.js
@@ -1,12 +1,17 @@
 export const putCate = async (userId, categoryId, categoryData) => {
   try {
-    const response = await fetch(`https://cookiee.site/category/${userId}/${categoryId}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(categoryData),
-    });
+    const response = await fetch(
+      `https://cookiee.site/category/${userId}/${categoryId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMiIsInJvbGUiOiJST0xFX1VTRVIiLCJpYXQiOjE3MDk1MTM4NzcsImV4cCI6MTcxMjEwNTg3N30.ZHC6ZPw6WsTfMR7at4FLkLAjNDU0vOMgfWL1lI3DsOs",
+        },
+        body: JSON.stringify(categoryData),
+      }
+    );
 
     if (!response.ok) {
       throw new Error("Network response was not ok");
