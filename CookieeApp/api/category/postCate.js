2697861a25e83a8b4427626bd4574e572f1fba2e
mxinseo <jade201913@gmail.com>
2024-03-05 01:51:07 +0900

    Fix: 카테고리 등록 헤더 토큰 추가

--- CookieeApp/api/category/postCate.js
+++ CookieeApp/api/category/postCate.js
@@ -2,7 +2,16 @@ import axios from "axios";
 
 export const postCate = async (userId, categoryData) => {
   try {
-    const response = await axios.post(`https://cookiee.site/category/${userId}`, categoryData);
+    const response = await axios.post(
+      `https://cookiee.site/category/${userId}`,
+      categoryData,
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMiIsInJvbGUiOiJST0xFX1VTRVIiLCJpYXQiOjE3MDk1MTM4NzcsImV4cCI6MTcxMjEwNTg3N30.ZHC6ZPw6WsTfMR7at4FLkLAjNDU0vOMgfWL1lI3DsOs",
+        },
+      }
+    );
 
     if (response.status !== 200) {
       throw new Error("Network response was not ok");
