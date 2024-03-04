3c1f182d177ec3ca0dc49326c5154158293b62dd
mxinseo <jade201913@gmail.com>
2024-03-05 01:48:55 +0900

    Fix: 썸네일 등록 헤더 토큰 추가

--- CookieeApp/api/thumbnail/createThumb.js
+++ CookieeApp/api/thumbnail/createThumb.js
@@ -23,6 +23,8 @@ export const createThumb = async (userId, selectedDate, asset) => {
       body: formData,
       headers: {
         "Content-Type": "multipart/form-data",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMiIsInJvbGUiOiJST0xFX1VTRVIiLCJpYXQiOjE3MDk1MTM4NzcsImV4cCI6MTcxMjEwNTg3N30.ZHC6ZPw6WsTfMR7at4FLkLAjNDU0vOMgfWL1lI3DsOs",
       },
     });
 
