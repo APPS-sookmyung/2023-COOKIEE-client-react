f25653f523a0838122cbe6a05ac366a7290400fb
mxinseo <jade201913@gmail.com>
2024-03-31 15:03:07 +0900

    Fix: index.js 수정

--- CookieeApp/app/index.js
+++ CookieeApp/app/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Text, Image, Pressable } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AppLoading from "expo-app-loading";
 import { StatusBar } from "expo-status-bar";
@@ -9,7 +9,7 @@ import Loading from "./screens/Loading";
 import User from "./screens/User";
 
 import "expo-router/entry";
-import { Link, Redirect, router } from "expo-router";
+import { Redirect } from "expo-router";
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -38,21 +38,19 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       {/* {isLoggedIn ? (
-          // 사용자가 로그인한 경우 메인 화면
-          <>
-            <Login onLogout={handleLogout} />
-            <User />
-          </>
-        ) : (
-          // 로그인 안 한 경우 로그인 화면 보여줌
-          <Login onLogin={handleLogin} />
-        )}
-        <StatusBar style="auto" /> */}
+        // 사용자가 로그인한 경우 메인 화면
+        <View>
+          <Login onLogout={handleLogout} />
+          <User />
+        </View>
+      ) : (
+        // 로그인 안 한 경우 로그인 화면 보여줌
+        <Login onLogin={handleLogin} />
+      )} */}
 
-      {/* <Redirect href={"home"} /> */}
-      <>
-        <Login />
-      </>
+      {/* <StatusBar style="auto" /> */}
+      <Redirect href={"home"} />
+      <Login />
     </SafeAreaView>
   );
 }
